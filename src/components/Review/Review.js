d06aa28a4ea21352b553bf937c1f414036ff3928
bvorel <bvorel@clickherelabs.com>
2022-08-16 14:42:21 -0500

    Added new star rating component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,7 +1,47 @@
 import styles from './Review.module.scss';
 import { classNames } from 'utils';
 
+import ReactStars from "react-rating-stars-component";
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+
 import Link from 'next/link';
+
+const ratingChanged = (newRating) => {
+  console.log(newRating);
+};
+
+const firstExample = {
+  size: 18,
+  count: 5,
+  color: "#e8e8e8",
+  activeColor: "#107f67",
+  edit: false,
+  value: 3.5,
+  a11y: true,
+  isHalf: true,
+  emptyIcon: <FaRegStar />,
+  halfIcon: <FaStarHalfAlt />,
+  filledIcon: <FaStar />,
+  onChange: newValue => {
+    console.log(`Example 2: new value is ${newValue}`);
+  }
+};
+const secondExample = {
+  size: 18,
+  count: 5,
+  color: "#e8e8e8",
+  activeColor: "#107f67",
+  edit: false,
+  value: 4.5,
+  a11y: true,
+  isHalf: true,
+  emptyIcon: <FaRegStar />,
+  halfIcon: <FaStarHalfAlt />,
+  filledIcon: <FaStar />,
+  onChange: newValue => {
+    console.log(`Example 2: new value is ${newValue}`);
+  }
+};
 
 export default function Review() {
   return (
@@ -12,6 +52,7 @@ export default function Review() {
             <img alt="" src="https://secure.gravatar.com/avatar/427dbff1e52ec95a065f5a6b34225c84?s=60&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/427dbff1e52ec95a065f5a6b34225c84?s=120&amp;d=mm&amp;r=g 2x" className={styles.avatar} height="60" width="60" loading="lazy"/>
             <div className={styles.reviewText}>
               <div className="star-rating" role="img" aria-label="Rated 3 out of 5">
+              <ReactStars {...firstExample} />
               </div>
               <p className={styles.reviewNameDate}>
                 <strong className={styles.reviewAuthor}>Ben </strong>
@@ -28,6 +69,7 @@ export default function Review() {
             <img alt="" src="https://secure.gravatar.com/avatar/427dbff1e52ec95a065f5a6b34225c84?s=60&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/427dbff1e52ec95a065f5a6b34225c84?s=120&amp;d=mm&amp;r=g 2x" className={styles.avatar} height="60" width="60" loading="lazy"/>
             <div className={styles.reviewText}>
               <div className="star-rating" role="img" aria-label="Rated 3 out of 5">
+              <ReactStars {...secondExample} />
               </div>
               <p className={styles.reviewNameDate}>
                 <strong className={styles.reviewAuthor}>Ben </strong>
