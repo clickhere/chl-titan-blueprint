09ce418cb7d52ca1f2372d9734078bb92034ddf4
Cory Hudson <chudson@clickherelabs.com>
2022-09-01 17:31:01 -0500

    Fix ProductSort bug due to using prop as default state

--- src/components/ProductSort/ProductSort.js
+++ src/components/ProductSort/ProductSort.js
@@ -1,51 +1,48 @@
 import styles from './ProductSort.module.scss';
 import { classNames } from 'utils';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import ProductSummary from 'components/ProductSummary';
 
 export default function ProductSort({ products }) {
-  const productsMap = products.map((product, index) => ({
-    product,
-    index,
-    price: (product.salePrice === 0 ? product.price : product.salePrice),
-    rating: product.reviewsRating,
-    popularity: product.totalSold,
-    latest: product.bigCommerceID,
-  }));
+  const productsMap = useMemo(() => (
+    products.map((product, index) => ({
+      product,
+      index,
+      price: (product.salePrice === 0 ? product.price : product.salePrice),
+      rating: product.reviewsRating,
+      popularity: product.totalSold,
+      latest: product.bigCommerceID,
+    }))
+  ), [products]);
   
-  const [sorted, setSorted] = useState({ order: 'index', products: productsMap });
+  const [sortOrder, setSortOrder] = useState('index');
   
-  function sortBy(order) {
-    setSorted({
-      order,
-      products: productsMap.slice().sort((a, b) => {
-        if (order === 'index') {
-          return a.index - b.index;
-        }
-        if (order === 'popularity') {
-          return b.popularity - a.popularity;
-        }
-        if (order === 'rating') {
-          return b.rating - a.rating;
-        }
-        if (order === 'latest') {
-          return b.latest - a.latest;
-        }
-        if (order === 'price-asc') {
-          return a.price - b.price;
-        }
-        if (order === 'price-desc') {
-          return b.price - a.price;
-        }
-        return 0;
-      })
-    });
-  }
+  const sorted = productsMap.slice().sort((a, b) => {
+    if (sortOrder === 'index') {
+      return a.index - b.index;
+    }
+    if (sortOrder === 'popularity') {
+      return b.popularity - a.popularity;
+    }
+    if (sortOrder === 'rating') {
+      return b.rating - a.rating;
+    }
+    if (sortOrder === 'latest') {
+      return b.latest - a.latest;
+    }
+    if (sortOrder === 'price-asc') {
+      return a.price - b.price;
+    }
+    if (sortOrder === 'price-desc') {
+      return b.price - a.price;
+    }
+    return 0;
+  });
   
   function SortUI() {
     return (
       <div className="column">
-        <select onChange={(event) => sortBy(event.target.value)} value={sorted.order}>
+        <select onChange={(event) => setSortOrder(event.target.value)} value={sortOrder}>
           <option value="index">Default sorting</option>
           <option value="popularity">Sort by popularity</option>
           <option value="rating">Sort by average rating</option>
@@ -62,7 +59,7 @@ export default function ProductSort({ products }) {
     <>
       <SortUI />
     
-      {sorted.products.map(({ product }) => (
+      {sorted.map(({ product }) => (
         <ProductSummary product={product} key={product.slug} />
       ))}
       
